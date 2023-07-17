8917aa395f2f3834e52bedf178ae38fb600b60a7
Orelbaz <orelbaz18@gmail.com>
2023-07-17 14:17:50 +0300

    new variables

--- K8-jenkins/Testing.sh
+++ K8-jenkins/Testing.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-TAG=$2
+TAG=$1
 INSTANCE_NAME="test"
 PROJECT_ID="gke-first-393008"
 ZONE="us-central1-c"
 
 
 echo 'Copying docker-compose.yml + .env to instance...'
-gcloud compute scp /var/lib/jenkins/workspace/K8-pipeline/Jenkins/K8-jenkins/CoinSite/docker-compose.yml [$INSTANCE_NAME]:/home/ec2-user/docker-compose.yml --project=[PROJECT_ID] --zone=[ZONE]
-gcloud compute scp /var/lib/jenkins/workspace/K8-pipeline/Jenkins/K8-jenkins/CoinSite/.env [$INSTANCE_NAME]:/home/ec2-user/docker-compose.yml --project=[PROJECT_ID] --zone=[ZONE]
+gcloud compute scp /var/lib/jenkins/workspace/K8-pipeline/Jenkins/K8-jenkins/CoinSite/docker-compose.yml $INSTANCE_NAME:/home/ec2-user/docker-compose.yml --project=$PROJECT_ID --zone=$ZONE
+gcloud compute scp /var/lib/jenkins/workspace/K8-pipeline/Jenkins/K8-jenkins/CoinSite/.env $INSTANCE_NAME:/home/ec2-user/.env --project=$PROJECT_ID --zone=$ZONE
+
 
 gcloud compute ssh --project=$PROJECT_ID --zone=$ZONE $INSTANCE_NAME \
 --ssh-flag="-o UserKnownHostsFile=/dev/null -o CheckHostIP=no -o StrictHostKeyChecking=no" \
