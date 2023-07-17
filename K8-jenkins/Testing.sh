57f0e086676d20258e44c72bf9d6ee8b6dcc902a
Orelbaz <orelbaz18@gmail.com>
2023-07-17 14:59:13 +0300

    Testing upadte

--- K8-jenkins/Testing.sh
+++ K8-jenkins/Testing.sh
@@ -27,12 +27,16 @@ sudo docker rmi \$(sudo docker images -q orelbaz/coinsiteK8)
 sudo docker-compose up -d
 "
 
-response=$(curl -s -o /dev/null -w "%{http_code}" ${TEST_IP}:5000)
+# Get test instance's IP
+INSTANCE_IP=$(gcloud compute instances describe $INSTANCE_NAME --zone=$ZONE --project=$PROJECT_ID --format='get(networkInterfaces[0].accessConfigs[0].natIP)')
 
-if [[ $response == 200 ]]; then
+# Test the http status
+http_response=$(curl -s -o /dev/null -w "%{http_code}" ${INSTANCE_IP}:5000)
+
+if [[ $http_response == 200 ]]; then
     echo "Flask app returned a 200 status code. Test passed!"
 else
-    echo "Flask app returned a non-200 status code: $response. Test failed!"
+    echo "Flask app returned a non-200 status code: $http_response. Test failed!"
     exit 1
 fi
 
