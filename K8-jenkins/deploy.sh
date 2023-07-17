a41272408721afcbc62c9447104b337b63536880
Or <134198819+Orelbaz@users.noreply.github.com>
2023-07-17 15:08:52 +0300

    Update deploy.sh

--- K8-jenkins/deploy.sh
+++ K8-jenkins/deploy.sh
@@ -18,6 +18,6 @@ sudo systemctl enable docker
 sudo systemctl start docker
 cd /home/ec2-user
 sudo docker-compose down
-sudo docker rmi \$(sudo docker images -q orelbaz/coinsiteK8)
+sudo docker rmi \$(sudo docker images -q orelbaz/coinsitek8)
 sudo docker-compose up -d
 "
