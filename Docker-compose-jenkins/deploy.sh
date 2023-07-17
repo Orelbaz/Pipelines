229b99d03d2e7db4fccd0afb2f4eaf58e1bdaa0a
Orelbaz <orelbaz18@gmail.com>
2023-07-17 09:56:55 +0300

    docker compose down

--- Docker-compose-jenkins/deploy.sh
+++ Docker-compose-jenkins/deploy.sh
@@ -16,11 +16,8 @@ sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-
 sudo chmod +x /usr/local/bin/docker-compose
 sudo systemctl enable docker
 sudo systemctl start docker
-sudo docker stop \$(sudo docker ps -aq)
-sudo docker rm \$(sudo docker ps -aq)
-sudo docker rmi \$(sudo docker images -q orelbaz/coinsite)
 cd /home/ec2-user
+sudo docker-compose down
+sudo docker rmi \$(sudo docker images -q orelbaz/coinsite)
 sudo docker-compose up -d
 "
-
-###
