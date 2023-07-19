8077262ae252c0d3e648cdfbf5bfcd1c9016c4d4
Orelbaz <orelbaz18@gmail.com>
2023-07-19 15:09:30 +0300

    sed tagg

--- K8-jenkins/deploy.sh
+++ K8-jenkins/deploy.sh
@@ -5,5 +5,5 @@ TAG=$1
 cd /var/lib/jenkins/workspace/K8-pipeline/Jenkins/K8-jenkins
 ###kubectl apply -f --set TAG=$TAG my-app-test.yaml
 
-envsubst < my-app-test.yaml | kubectl apply -f -
+sed "s/\${TAG}/$TAG/g" my-app-test.yaml | kubectl apply -f -
 
