51d39c2e34295dfbc04c4a483f1b3f3f971b53bc
Orelbaz <orelbaz18@gmail.com>
2023-07-19 14:53:31 +0300

    envsubst test

--- K8-jenkins/deploy.sh
+++ K8-jenkins/deploy.sh
@@ -3,4 +3,7 @@
 TAG=$1
 
 cd /var/lib/jenkins/workspace/K8-pipeline/Jenkins/K8-jenkins
-kubectl apply -f --set TAG=$TAG my-app-test.yaml
+###kubectl apply -f --set TAG=$TAG my-app-test.yaml
+
+envsubst < my-app-test.yaml | kubectl apply -f -
+
