1db34126a4dca31edeea3b704704e0e0dd944dab
Orelbaz <orelbaz18@gmail.com>
2023-07-19 15:19:25 +0300

    Clean up

--- K8-jenkins/deploy.sh
+++ K8-jenkins/deploy.sh
@@ -3,7 +3,5 @@
 TAG=$1
 
 cd /var/lib/jenkins/workspace/K8-pipeline/Jenkins/K8-jenkins
-###kubectl apply -f --set TAG=$TAG my-app-test.yaml
-
-sed "s/\${TAG}/$TAG/g" my-app-test.yaml | kubectl apply -f -
+sed "s/\${TAG}/$TAG/g" my-app.yaml | kubectl apply -f -
 
