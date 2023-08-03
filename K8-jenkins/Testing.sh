27413ed55ee9b0c450cf2bdeb7821522068bab14
Orelbaz <orelbaz18@gmail.com>
2023-08-03 10:02:15 +0300

    Test cluster change

--- K8-jenkins/Testing.sh
+++ K8-jenkins/Testing.sh
@@ -1,29 +1,10 @@
 #!/bin/bash
 
-INSTANCE_NAME="k8-test"
-PROJECT_ID="gke-first-393008"
-ZONE="us-central1-c"
+TAG=$1
 
+cd /var/lib/jenkins/workspace/K8-pipeline/Jenkins/K8-jenkins
+sed "s/\${TAG}/$TAG/g" my-app.yaml | kubectl apply -f -
 
-echo 'Copying docker-compose.yml + .env to instance...'
-gcloud compute scp /var/lib/jenkins/workspace/K8-pipeline/Jenkins/K8-jenkins/CoinSite/docker-compose.yml $INSTANCE_NAME:/home/jenkins/docker-compose.yml --project=$PROJECT_ID --zone=$ZONE
-gcloud compute scp /var/lib/jenkins/workspace/K8-pipeline/Jenkins/K8-jenkins/CoinSite/.env $INSTANCE_NAME:/home/jenkins/.env --project=$PROJECT_ID --zone=$ZONE
-
-
-gcloud compute ssh --project=$PROJECT_ID --zone=$ZONE $INSTANCE_NAME \
---ssh-flag="-o UserKnownHostsFile=/dev/null -o CheckHostIP=no -o StrictHostKeyChecking=no" \
---command "
-
-sudo apt-get update && sudo apt-get install docker.io -y
-sudo curl -L 'https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)' -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-sudo systemctl enable docker
-sudo systemctl start docker
-cd /home/jenkins
-sudo docker-compose down
-sudo docker rmi \$(sudo docker images -q orelbaz/coinsitek8)
-sudo docker-compose up -d
-"
 
 # Get test instance's IP
 INSTANCE_IP=$(gcloud compute instances describe $INSTANCE_NAME --zone=$ZONE --project=$PROJECT_ID --format='get(networkInterfaces[0].accessConfigs[0].natIP)')
