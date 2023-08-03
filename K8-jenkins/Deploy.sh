b0778822504cf3ae38297140f817de420763e20d
Orelbaz <orelbaz18@gmail.com>
2023-08-03 10:42:30 +0300

    Test Cluster

--- K8-jenkins/Deploy.sh
+++ K8-jenkins/Deploy.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 TAG=$1
+CLUSTER=$2
+
+export USE_GKE_GCLOUD_AUTH_PLUGIN=True
+gcloud container clusters get-credentials --project gke-first-393008 $CLUSTER --zone us-central1-c
 
 cd /var/lib/jenkins/workspace/K8-pipeline/Jenkins/K8-jenkins
 sed "s/\${TAG}/$TAG/g" my-app.yaml | kubectl apply -f -
