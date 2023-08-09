141680ecad80e67e45613be3db495c658262e90a
Orelbaz <orelbaz18@gmail.com>
2023-08-09 10:48:14 +0300

    Delete promethues vaules

--- K8-jenkins/Prometheus.sh
+++ K8-jenkins/Prometheus.sh
@@ -3,5 +3,5 @@
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
 
-helm install prometheus prometheus-community/prometheus -f prometheus-values.yaml
+helm install prometheus prometheus-community/prometheus
 
