90a5e3f4a2c867d423c9b3e3c3fe04b1857d26c0
Orelbaz <orelbaz18@gmail.com>
2023-08-14 09:30:21 +0300

    change name all

--- Ansible-pipeline/deploy_script.sh
+++ Ansible-pipeline/deploy_script.sh
@@ -2,4 +2,4 @@
 sudo yum install python -y
 sudo yum install python-pip -y
 sudo pip install ansible
-ansible-playbook /home/ec2-user/Jenkins/Ansible-jenkins/ansible/flask-playbook.yml 
+ansible-playbook /home/ec2-user/Jenkins/Ansible-pipeline/ansible/flask-playbook.yml 
