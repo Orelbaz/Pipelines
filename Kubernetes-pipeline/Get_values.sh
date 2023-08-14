7b1bd5bbb1e30d3a915a4212f026d38e05afbac8
Orelbaz <orelbaz18@gmail.com>
2023-08-14 12:35:43 +0300

    0 instead 1

--- Kubernetes-pipeline/Get_values.sh
+++ Kubernetes-pipeline/Get_values.sh
@@ -3,7 +3,7 @@
 echo 'Getting Chart.yaml...'
 echo "apiVersion: v2
 name: stock-site
-version: 1.$BUILD_NUMBER.0
+version: 0.$BUILD_NUMBER.0
 description: A Helm chart for deploying Flask and Redis" > Chart.yaml
 
 echo 'Getting values.yaml...'
