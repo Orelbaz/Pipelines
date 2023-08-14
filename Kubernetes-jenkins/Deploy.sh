0a3e2165caf834bbd861f1e0a3f78b9b8cad7e50
Orelbaz <orelbaz18@gmail.com>
2023-08-14 09:26:16 +0300

    Change to kubernetes name

--- Kubernetes-jenkins/Deploy.sh
+++ Kubernetes-jenkins/Deploy.sh
@@ -8,7 +8,7 @@ MY_PATH=$3
 export USE_GKE_GCLOUD_AUTH_PLUGIN=True
 gcloud container clusters get-credentials --project gke-first-393008 $CLUSTER --zone us-central1-c
 
-cd ${MY_PATH}K8-jenkins/Helm-chart
+cd ${MY_PATH}Kubernetes-pipeline/Helm-chart
 helm package .
 
 
@@ -33,7 +33,7 @@ if [[ $CLUSTER == "eks-test" ]]; then
     if [[ $http_response == 200 ]]; then
         echo "Flask app returned a 200 status code. Test passed!"
         echo "Uploading Helm chart to the Google Cloud Storage bucket"
-        gsutil cp ${MY_PATH}K8-jenkins/Helm-chart/stock-site-0.2.0.tgz gs://stock-site
+        gsutil cp ${MY_PATH}Kubernetes-pipeline/Helm-chart/stock-site-0.2.0.tgz gs://stock-site
     else
         echo "Flask app returned a non-200 status code: $http_response. Test failed!"
         exit 1
